d6d2e214925a7f5952f5a0fef7e9d62331c6e866
Mike Bakker <equanoxproductions@gmail.com>
2018-01-14 03:52:29 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars = [
+  "snickers", "hundred grand", "kitkat", "skittles"]
+  function addElementToBeginningOfArray() {
+    chocolateBars.push();
+    chocolateBars.unshift();
+  }
