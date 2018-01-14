6f7339bc5c720d0f11815fd30d3be7a395393e15
Mike Bakker <equanoxproductions@gmail.com>
2018-01-14 04:24:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = [
   function addElementToBeginningOfArray(array, element) {
     return [element, ...array]
   }
-  function destructivelyAddElementsToBeginningOfArray(array, element) {
+  function destructivelyAddElementToBeginningOfArray(array, element) {
     array.unshift(element)
     return array
   }
