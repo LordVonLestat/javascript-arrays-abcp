87fad485aa2aee491b7c35adfc5282ab5ce189ec
Mike Bakker <equanoxproductions@gmail.com>
2018-01-14 04:09:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = [
   "snickers", "hundred grand", "kitkat", "skittles"]
   function addElementToBeginningOfArray() {
-   var array.push("element");
-    array.unshift("element");
+   chocolateBars.push("element");
+   chocolateBars.unshift("element");
   }
